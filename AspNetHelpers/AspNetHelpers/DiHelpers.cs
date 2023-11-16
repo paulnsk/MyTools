3509bab3cfc93f669e3d714cab0af14fe41b033f
-paulnsk- <paul.nsk@gmail.com>
2023-11-17 01:37:59 +0700

    added DiHelpers

---
+++ AspNetHelpers/AspNetHelpers/DiHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace AspNetHelpers
+{
+    public static class DiHelpers
+    {
+        public static T GetJustRegisteredService<T>(this IServiceCollection services) where T : class
+        {
+            return services.BuildServiceProvider().GetRequiredService<T>();
+        }
+
+
+        public static T AddConfigOptions<T>(this IServiceCollection services, Action<T>? configAction = default) where T : class
+        {
+
+            var optionsBuilder = services.AddOptions<T>()
+                .BindConfiguration(nameof(T));
+            if (configAction != null) optionsBuilder.Configure(configAction);
+            optionsBuilder.ValidateDataAnnotations();
+            return services.GetJustRegisteredService<IOptions<T>>().Value;
+        }
+    }
+}
